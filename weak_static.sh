55ad29a3702fbab774d0427d78b0989fd0d8baa5
ldealy98 <38056466+ldealy98@users.noreply.github.com>
2020-10-30 18:41:49 -0400

    Add files via upload

---
+++ weak_static.sh
@@ -0,0 +1,3 @@
+# In this file you will write your weak scalability test that increases the number of
+# temperature values tested in the suite proportionate to the number of threads for
+# 1, 2, 4, 8, 16 and 32 threads. This will be run with static scheduling.
